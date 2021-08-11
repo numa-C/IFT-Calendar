fa29623ef65904a48790137c6ca1d8a7886866c0
RyoKainuma <kr49kr21@gmail.com>
2021-08-12 07:16:51 +0900

    add local package.

---
+++ src/handler/handler.go
@@ -0,0 +1,13 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func MainPage() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello World")
+	}
+}
